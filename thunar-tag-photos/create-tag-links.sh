20e625173d221e693eba3bf5f8974f20a2ff512f
Stuart J Mackintosh <sjm@opusvl.com>
2020-05-25 14:21:21 +0100

    Added notes relating to performance

--- thunar-tag-photos/create-tag-links.sh
+++ thunar-tag-photos/create-tag-links.sh
@@ -14,16 +14,18 @@ COUNT=0
 
 ## find all jpgs, for each with any tags, store the name,  split the tags, create a folder as the tag name, create symlinks to each image under
 
+
 for IMAGEFILE in `find $IMAGEBASE -iname *.jpg`
  do
 	COUNT=$((COUNT + 1))
 	TAGS=`exiftool -q -q -Keywords $IMAGEFILE |cut -f 2 -d :`
+# TODO: This would be faster if we had an index with filename and mod date so we only examine changed files for tags
 
 	if [ ! -z "${TAGS}" ]
 	 then
 		TAG_LIST=`echo $TAGS |tr , "\n" `
 		for TAG in $TAG_LIST
-# TODO: Bug - when tas have spaces, all parts are listed, so need to use he comma as separator, not re-split
+# TODO: Bug - when tags have spaces, all parts are listed, so need to use a comma as separator, not re-split, and set/reset IFS
 		 do
 			LINKDIR=$LINKBASE/${TAG//[^A-Za-z0-9._-]/_}
 			LINKDEST=$LINKDIR/`basename $IMAGEFILE`
