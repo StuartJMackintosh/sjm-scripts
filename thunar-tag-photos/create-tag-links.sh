d62fe1458e52cdc8de5aefd59af8cbf529a06e28
Stuart J Mackintosh <sjm@opusvl.com>
2020-05-25 13:44:37 +0100

    Added readme, added script to create symlinks

---
+++ thunar-tag-photos/create-tag-links.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+BASEPATH=/srv/fileshare/Media/Photos
+
+IMAGEBASE=$BASEPATH
+#IMAGEBASE=$BASEPATH/20171022/
+
+LINKBASE=$BASEPATH/tag-index
+
+
+# TODO: Check we have bins installed (exiftool etc)
+
+COUNT=0
+
+## find all jpgs, for each with any tags, store the name,  split the tags, create a folder as the tag name, create symlinks to each image under
+
+for IMAGEFILE in `find $IMAGEBASE -iname *.jpg`
+ do
+	COUNT=$((COUNT + 1))
+	TAGS=`exiftool -q -q -Keywords $IMAGEFILE |cut -f 2 -d :`
+
+	if [ ! -z "${TAGS}" ]
+	 then
+		TAG_LIST=`echo $TAGS |tr , "\n" `
+		for TAG in $TAG_LIST
+# TODO: Bug - when tas have spaces, all parts are listed, so need to use he comma as separator, not re-split
+		 do
+			LINKDIR=$LINKBASE/${TAG//[^A-Za-z0-9._-]/_}
+			LINKDEST=$LINKDIR/`basename $IMAGEFILE`
+
+			mkdir -p $LINKDIR
+
+			if [ ! -f $LINKDEST ]
+			 then
+				echo " - Linking $IMAGEFILE to $LINKDEST"
+				ln -s $IMAGEFILE $LINKDEST
+			fi
+		done
+	fi
+done
+
+
+
+
